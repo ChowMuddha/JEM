adfa9c226640148c653b537ddca941c02e291eb4
ChowMuddha <102262010+ChowMuddha@users.noreply.github.com>
2022-06-09 08:43:44 +1000

    Add files via upload

---
+++ DooleysServertoJEM.ps1
@@ -0,0 +1,34 @@
+#Dooleys to JEM Automate Migration
+Set-ExecutionPolicy Unrestricted -Force
+
+#Removed old instances of control
+#Will need to add mutiple lines for Screenconnect depending on how many versions or instances are installed on the PC
+#wmic product where name="ScreenConnect Client (xxxxxxxxxxxxxxxx)" call uninstall /nointeractive
+
+#Creates Temp Directory on C:\
+New-Item -Path 'C:\JEM\' -ItemType Directory
+
+#Download Automate Uninstaller
+Invoke-WebRequest -Uri "https://s3.amazonaws.com/assets-cp/assets/Agent_Uninstaller.zip" -OutFile C:\JEM\Agent_Uninstall.zip;
+#Extract Uninstaller
+Expand-Archive -LiteralPath 'C:\JEM\Agent_Uninstall.zip' -DestinationPath C:\JEM\
+#Delete .zip file
+$RemovalPath = "C:\JEM\Agent_Uninstall.zip"
+Remove-Item $RemovalPath -Force -Recurse
+
+#Will need to generate new agent download URL and update below based offsite location
+Invoke-WebRequest -Uri "https://automate.jem.com.au/LabTech/Deployment.aspx?InstallerToken=4944bf03882449249930f75ea3b2bc3f" -OutFile "C:\JEM\Dooleys_ServersJEM.MSI";
+
+#Executes Silent removal of old Dooleys Agent
+Invoke-Command -ScriptBlock {Start-Process "C:\JEM\Agent_Uninstall.exe" -ArgumentList "/q" -Wait} 
+Start-Sleep -s 15
+
+#Installs JEM Agent
+msiexec /i C:\JEM\Dooleys_ServersJEM.MSI /quiet /qn /norestart /log c:\AgentMigration.log
+
+#Removes Files from temp directory and files
+$RemovalPath = "C:\JEM\Agent_Uninstaller.zip"
+Get-ChildItem  -Path $RemovalPath -Recurse  | Remove-Item -Force -Recurse
+Remove-Item $RemovalPath -Force -Recurse
+
+exit
